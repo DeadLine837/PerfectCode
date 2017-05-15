5f250efd3d853e31b266ed0c51079048b42728a4
DeadLine837 <970686446@qq.com>
2017-05-15 13:50:06 +0800

    Update Codecollect.java

--- Codecollect.java
+++ Codecollect.java
@@ -1,1 +1,7 @@
-源码大招：不服来战！撸这些完整项目，你不牛逼都难！  http://www.54tianzhisheng.cn/2017/05/13/android-projects/
+源码大招：不服来战！撸这些完整项目，你不牛逼都难！   http://www.54tianzhisheng.cn/2017/05/13/android-projects/
+干货来了，19个完整app正在赶来，不看你会后悔的       https://mp.weixin.qq.com/s/ydhmfP3zHIdoAfuRcyEAew
+菜单开源库装逼大全   https://mp.weixin.qq.com/s/k12Wq6BS2FX2LpSaLvnPWg
+手把手教你从零开始做一个好看的 APP     http://mp.weixin.qq.com/s/jOW40fpAlNGvrOfQe9vJyA
+
+
+
